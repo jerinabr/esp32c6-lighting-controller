1bf5cbc4ba5f32aadf6dcaf8a586178e4a68e564
Jerin Abraham <abrahamjerin98@gmail.com>
2026-09-20 02:57:56 +0000

    Remove float math from LED strip handler
    - Found out the ESP32C6 doesn't have an FPU so all my float math was
      happening really inefficiently
    - Replaced all the float math in the LED strip handler with byte math
      and fixed point math
    - Changed the interpolation loop to use an interpolation step instead of
      using the max delta as the stop
    - Added a gamma and sin LUT to speed up calculations

--- components/led_strip_handler/src/led_strip_handler.c
+++ components/led_strip_handler/src/led_strip_handler.c
@@ -4,7 +4,6 @@
 #include "esp_log.h"
 #include "freertos/idf_additions.h"
 #include "led_strip.h"
-#include "math.h"
 
 /*
     Message tag used for ESP_LOGI messages
@@ -16,6 +15,146 @@
 */
 #define INITIAL_BRIGHTNESS  32
 #define INITIAL_COLOR_TEMP  0
+
+/*
+    Gamma LUT
+*/
+static const uint8_t GAMMA_LUT[256] = {
+	0, 0, 0, 0,
+	0, 0, 0, 0,
+	0, 0, 0, 0,
+	1, 1, 1, 1,
+	1, 1, 1, 1,
+	2, 2, 2, 2,
+	2, 2, 3, 3,
+	3, 3, 4, 4,
+	4, 4, 5, 5,
+	5, 5, 6, 6,
+	6, 7, 7, 7,
+	8, 8, 8, 9,
+	9, 9, 10, 10,
+	11, 11, 11, 12,
+	12, 13, 13, 14,
+	14, 15, 15, 16,
+	16, 17, 17, 18,
+	18, 19, 19, 20,
+	20, 21, 21, 22,
+	23, 23, 24, 24,
+	25, 26, 26, 27,
+	28, 28, 29, 30,
+	30, 31, 32, 32,
+	33, 34, 35, 35,
+	36, 37, 38, 38,
+	39, 40, 41, 42,
+	42, 43, 44, 45,
+	46, 47, 47, 48,
+	49, 50, 51, 52,
+	53, 54, 55, 56,
+	56, 57, 58, 59,
+	60, 61, 62, 63,
+	64, 65, 66, 67,
+	68, 69, 70, 71,
+	73, 74, 75, 76,
+	77, 78, 79, 80,
+	81, 82, 84, 85,
+	86, 87, 88, 89,
+	91, 92, 93, 94,
+	95, 97, 98, 99,
+	100, 102, 103, 104,
+	105, 107, 108, 109,
+	111, 112, 113, 115,
+	116, 117, 119, 120,
+	121, 123, 124, 126,
+	127, 128, 130, 131,
+	133, 134, 136, 137,
+	139, 140, 142, 143,
+	145, 146, 148, 149,
+	151, 152, 154, 155,
+	157, 158, 160, 162,
+	163, 165, 166, 168,
+	170, 171, 173, 175,
+	176, 178, 180, 181,
+	183, 185, 186, 188,
+	190, 192, 193, 195,
+	197, 199, 200, 202,
+	204, 206, 207, 209,
+	211, 213, 215, 217,
+	218, 220, 222, 224,
+	226, 228, 230, 232,
+	233, 235, 237, 239,
+	241, 243, 245, 247,
+	249, 251, 253, 255
+};
+
+/*
+    Sin LUT to speed up interpolation
+*/
+static const uint8_t SIN_LUT_0_90[256] = {
+	0, 2, 3, 5,
+	6, 8, 9, 11,
+	13, 14, 16, 17,
+	19, 20, 22, 24,
+	25, 27, 28, 30,
+	31, 33, 34, 36,
+	38, 39, 41, 42,
+	44, 45, 47, 48,
+	50, 51, 53, 55,
+	56, 58, 59, 61,
+	62, 64, 65, 67,
+	68, 70, 71, 73,
+	74, 76, 77, 79,
+	80, 82, 83, 85,
+	86, 88, 89, 91,
+	92, 94, 95, 96,
+	98, 99, 101, 102,
+	104, 105, 107, 108,
+	109, 111, 112, 114,
+	115, 116, 118, 119,
+	121, 122, 123, 125,
+	126, 127, 129, 130,
+	132, 133, 134, 136,
+	137, 138, 140, 141,
+	142, 143, 145, 146,
+	147, 149, 150, 151,
+	152, 154, 155, 156,
+	157, 159, 160, 161,
+	162, 164, 165, 166,
+	167, 168, 169, 171,
+	172, 173, 174, 175,
+	176, 178, 179, 180,
+	181, 182, 183, 184,
+	185, 186, 187, 188,
+	190, 191, 192, 193,
+	194, 195, 196, 197,
+	198, 199, 200, 201,
+	202, 203, 203, 204,
+	205, 206, 207, 208,
+	209, 210, 211, 212,
+	213, 213, 214, 215,
+	216, 217, 218, 218,
+	219, 220, 221, 222,
+	222, 223, 224, 225,
+	225, 226, 227, 228,
+	228, 229, 230, 230,
+	231, 232, 232, 233,
+	234, 234, 235, 235,
+	236, 237, 237, 238,
+	238, 239, 239, 240,
+	241, 241, 242, 242,
+	243, 243, 243, 244,
+	244, 245, 245, 246,
+	246, 247, 247, 247,
+	248, 248, 248, 249,
+	249, 249, 250, 250,
+	250, 251, 251, 251,
+	251, 252, 252, 252,
+	252, 253, 253, 253,
+	253, 253, 254, 254,
+	254, 254, 254, 254,
+	254, 255, 255, 255,
+	255, 255, 255, 255,
+	255, 255, 255, 255,
+};
 
 struct led_state_s {
     uint8_t brightness;
@@ -31,7 +170,54 @@ static uint32_t config_num_leds = 0;
 *******************************************************************************/
 
 /*!
+    @brief Scale a number by an alpha value
+
+    @param alpha Number that is used for scaling (0 - 255)
+    @param n Number that will be scaled (0 - 255)
+
+    @return Results of alpha scaling
+
+    @details The calculation here is equivalent to
+        y = (alpha / 255) * n
+
+    The calculations were taken from this arXiv paper:
+    https://arxiv.org/html/2202.02864v2#S2
+*/
+static uint8_t alpha_blend(uint8_t alpha, uint8_t n) {
+    uint32_t a = alpha * n;
+    a += 0x80U;
+    a += (a >> 8);
+    return (a >> 8);
+}
+
+/*!
+    @brief Linear interpolate between 2 numbers given an alpha
+
+    @param start Starting value (0 - 255)
+    @param end Ending value (0 - 255)
+    @param alpha Interpolation alpha (0 - 255)
+
+    @returns Interpolated result
+
+    @details The calculation is equivalent to
+        y = a + (alpha / 255) * (b - a)
+*/
+static uint8_t interpolate(uint8_t start, uint8_t end, uint8_t alpha) {
+    uint8_t a_i = alpha_blend(255 - alpha, start);
+    uint8_t b_i = alpha_blend(alpha, end);
+    uint16_t result = (uint16_t) a_i + (uint16_t) b_i;
+    return (result > 255 ? 255 : result);
+}
+
+/*!
     @brief Initialize the LED strip
+
+    @param gpio_pin GPIO pin the LED strip is connected to
+    @param num_leds Number of LEDs in the strip
+
+    @return
+    - ESP_OK if the LED strip successfully initialized
+    - error codes if the LED strip failed to initialize
 */
 static esp_err_t led_strip_configure(uint32_t gpio_pin, uint32_t num_leds) {
     /* Create LED strip configuration */
@@ -96,25 +282,22 @@ static void set_led_strip(
     @brief Set the LED strip pixels based on a normalized brightness and color
     temperature and apply gamma correction
 
-    @param brightness Normalized brightness (0.0 - 1.0)
-    @param color_temp Normalized color temperature (0.0 - 1.0)
+    @param brightness Normalized brightness (0 - 255)
+    @param color_temp Normalized color temperature (0 - 255)
 
     @details The function assumes a gamma correction factor of 2. With that, the
     basic white value calculations are as follows:
-        cool_white = 255 * brightness ^ 2 * color_temp
-        warm_white = 255 * brightness ^ 2 * (1 - color_temp)
+        warm_white = (brightness / 255) ^ gamma * (255 - color_temp)
+        cool_white = (brightness / 255) ^ gamma * color_temp
 */
 static void set_led_strip_from_norm_led_state(
     led_strip_handle_t led_strip,
-    float brightness,
-    float color_temp
+    uint8_t brightness,
+    uint8_t color_temp
 ) {
-    float b_2 = brightness * brightness;
-    float ww = b_2 * (1 - color_temp);
-    float cw = b_2 * color_temp;
-
-    uint8_t warm_white = roundf(255 * ww);
-    uint8_t cool_white = roundf(255 * cw);
+    uint8_t g_b = GAMMA_LUT[brightness];
+    uint8_t warm_white = alpha_blend(g_b, 255 - color_temp);
+    uint8_t cool_white = alpha_blend(g_b, color_temp);
     set_led_strip(led_strip, warm_white, cool_white);
 }
 
@@ -125,33 +308,28 @@ static void interpolate_led_strip(
 ) {
     /* Override the brightness if the LED is actually supposed to be off since
         the brightness value doesn't change for that */
-    uint8_t actual_brightness = led_state_req->brightness;
+    uint8_t actual_req_brightness = led_state_req->brightness;
     if (led_state_req->power != 1) {
-        actual_brightness = 0;
+        actual_req_brightness = 0;
     }
 
     /* Interpolating brightness and color temperature are 2 independent
         operations, so the largest delta is used to determine how fast the
         interpolation should be */
-    uint8_t delta_brightness = abs(actual_brightness - current_led_state->brightness);
+    uint8_t delta_brightness = abs(actual_req_brightness - current_led_state->brightness);
     uint8_t delta_color_temp = abs(led_state_req->color_temp - current_led_state->color_temp);
     uint8_t max_delta = MAX(delta_brightness, delta_color_temp);
 
-    /* Pre-compute the normalized brightness and color temperature values used
-        for the linear interpolation because these values don't change during
-        the interpolation steps */
-    float b0_norm = current_led_state->brightness / 255.0;
-    float ct0_norm = current_led_state->color_temp / 255.0;
-    float b1_norm = actual_brightness / 255.0;
-    float ct1_norm = led_state_req->color_temp / 255.0;
-    float b1_0_norm = b1_norm - b0_norm;
-    float ct1_0_norm = ct1_norm - ct0_norm;
-    float b_i = b0_norm;
-    float ct_i = ct0_norm;
-    uint8_t interpolation_interrupted = 0;
-    for (int i = 1; i <= max_delta; i += 8) {
-        vTaskDelay(pdMS_TO_TICKS(20));
+    /* Calculate the interpolation step with the formula
+        y = (4 * max_delta + 1023) / 128
+        This lets the step size be proportional to the max delta, but it ensures
+        that the smallest step size is 8 and the largest is 15. */
+    uint32_t interpolation_step = ((max_delta << 2) + 1024) >> 7;
 
+    uint8_t b_i = current_led_state->brightness;
+    uint8_t ct_i = current_led_state->color_temp;
+    uint8_t interpolation_interrupted = 0;
+    for (int i = interpolation_step; i < 256; i += interpolation_step) {
         /* Check if there's any new LED state requests in the queue. If there
             are, then stop this interpolation so the next one can start
             immediately. */
@@ -159,18 +337,29 @@ static void interpolate_led_strip(
             interpolation_interrupted = 1;
             break;
         }
-        
-        /* Calculate intermediate normalized brightness and color temperature */
-        float a = i / (float) max_delta;
-        a = sinf(M_PI_2 * a);
-        b_i = b0_norm + a * b1_0_norm;
-        ct_i = ct0_norm + a * ct1_0_norm;
+
+        /* Use sine interpolation for a smoother fade */
+        uint8_t alpha = SIN_LUT_0_90[i];
+
+        /* Interpolate brightness and color temperature */
+        b_i = interpolate(
+            current_led_state->brightness,
+            actual_req_brightness,
+            alpha
+        );
+        ct_i = interpolate(
+            current_led_state->color_temp,
+            led_state_req->color_temp,
+            alpha
+        );
 
         set_led_strip_from_norm_led_state(
             led_strip,
             b_i,
             ct_i
         );
+
+        vTaskDelay(pdMS_TO_TICKS(20));
     }
 
     if (interpolation_interrupted) {
@@ -178,20 +367,19 @@ static void interpolate_led_strip(
             LED state request, the LED state is updated with the last
             interpolated value so that the next interpolation starts from where
             this one left off */
-        current_led_state->brightness = 255 * b_i;
-        current_led_state->color_temp = 255 * ct_i;
+        current_led_state->brightness = b_i;
+        current_led_state->color_temp = ct_i;
     } else {
         /* If the interpolation completed, then set the LED strip and LED state
             to the final brightness and color temperature values. This is done
-            because the interpolation loop end might not be a multiple of the
-            interpolation step. */
+            because the interpolation step might not be a factor of 256. */
         set_led_strip_from_norm_led_state(
             led_strip,
-            b1_norm,
-            ct1_norm
+            actual_req_brightness,
+            led_state_req->color_temp
         );
 
-        current_led_state->brightness = actual_brightness;
+        current_led_state->brightness = actual_req_brightness;
         current_led_state->color_temp = led_state_req->color_temp;
     }
 }
